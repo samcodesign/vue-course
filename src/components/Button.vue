290650aa7333906274142726c591d8c9f1c41cca
samy benmahrez <samyben152@gmail.com>
2022-06-27 16:51:34 +0200

    .

---
+++ src/components/Button.vue
@@ -0,0 +1,18 @@
+<template>
+    <button :style="{background: color}" @click="onClick()" class="btn">{{text}}</button>
+</template>
+
+<script>
+    export default {
+        name:'Button',
+        props: {
+            text: String, 
+            color: String,
+        },
+        methods:{
+            onClick() {
+                console.log('click')
+            }
+        }
+    }
+</script>
